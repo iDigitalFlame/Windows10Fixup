fc62d4918f8fcb336b2fd6ce6706664134c835e5
iDigitalFlame <idf@idigitalflame.com>
2020-04-13 09:27:06 -0400

    Added error action global

--- FixWin10.ps1
+++ FixWin10.ps1
@@ -18,6 +18,8 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #
 
+$ErrorActionPreference = "SilentlyContinue"
+
 function mkdir($dir) {
     if (Test-Path $dir) {
         return
